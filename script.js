fafaa7e03deaa171e9db8b84cf2d078c91c7a8bb
Andrey <example@gmail.ru>
2019-03-03 15:16:11 +0700

    project_1

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-"use strict";
+let money = prompt("Ваш бюджет на месяц", ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let answer = prompt("Are you here?", "yes");
+let appData = {
+  budget: money,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  timeData: time,
+  saving: false
+}
 
-console.log(answer);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
